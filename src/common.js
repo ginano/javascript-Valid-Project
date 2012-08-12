6c0b64dc6eefe4a200e8a8d3eb5c6f8b93d2076f
ginano <hustestor@gmail.com>
2012-08-12 14:36:24 +0800

    update

--- src/common.js
+++ src/common.js
@@ -1,5 +1,17 @@
-var isDebug=/debug/i.test(window.location.hash);
-(function log(window){
+var isDebug=/debug/i.test(window.location.hash),
+	Menus=[
+		{
+			'url':'function_declare.html',
+			'name':'函数声明与函数表达式',
+			'title':'测试函数声明与表达式的一些基本特征'
+		},
+		{
+			'url':'global_var_delete.html',
+			'name':'全局变量的声明和未声明的区别',
+			'title':'全局变量的声明和未声明的区别'
+		}
+	];
+(function(window){
 	var loginfos=[],
 		_log=function(str){
 			var html, ol; //是否开启debug模式仅仅在控制台显示
@@ -27,15 +39,77 @@ var isDebug=/debug/i.test(window.location.hash);
 			}else{
 				return '';
 			}
+		},
+		/**
+		 * 去除子元素影响
+		 * @param {Object} fun
+		 */
+		_withinEl=function(fun){
+			return function(e){
+				var parent=e.relatedTarget;//上一次相应mouseover/out的元素
+				while(parent!=this&&parent){
+					try{
+						parent=parent.parentNode;
+					}catch(e){
+						break;
+					}
+				}
+				if(parent!=this){
+					fun(e);
+				}
+			};
+		},
+		_addEvent=function(el,event,fun){
+			if(el.attachEvent){
+				el.attachEvent('on'+event,fun);
+			}else if(el.addEventListener){
+				switch(event){
+					case 'mouseenter':
+						el.addEventListener('mouseover',_withinEl(fun),false);
+						break;
+					case 'mouseleave':
+						el.addEventListener('mouseout',_withinEl(fun),false);
+						break;
+					default:
+						el.addEventListener(event,fun,false);
+						break;
+				}
+				
+			}else{
+				el['on'+event]=fun;
+			}
+			
+		},
+		_removeEvent=function(el,event,fun){
+			if(el.removeEventListener){
+				switch(event){
+					case 'mouseenter':
+						event='mouseover';
+						break;
+					case 'mouseleave':
+						event='mouseout';
+						break;
+					default:
+						break; 
+				}
+				el.removeEventListener(event,fun,false);
+			}else if(el.detachEvent){
+				el.detachEvent('on'+event,fun);
+			}else{
+				el['on'+event]=null;
+			}
 		};
 	window.log=_log;
 	window.getLog=_getLog;
+	window.addEvent=_addEvent;
+	window.removeEvent=_removeEvent;
 })(window);
 window.onload=function(){
 	var init={
 		done:function(){
 			this.showSource();
 			this.declare(); //最后才免责声明
+			this.showMenu();//菜单显示
 		},
 		declare:function(){
 			var html=document.createElement('div');
@@ -70,6 +144,39 @@ window.onload=function(){
 			html.innerHTML=_html.join('');				
 			document.body.appendChild(html);
 			
+		},
+		showMenu:function(){
+			var html=document.createElement('div'),
+				i,len,temp,_html=[],cls='',
+				menuList,
+				arrow,
+				url=location.href;
+			html.setAttribute('id','testcase-menus');
+			_html.push('<ul id="menu-list" class="menu-list">');
+			for(i=0,len=Menus.length;i<len;i++){
+				temp=Menus[i];
+				if(url.indexOf(temp['url'])>-1){
+					cls='current';
+				}else{
+					cls='';
+				}
+				_html.push('<li><a class=" '+cls+' " href="'+temp['url']+'" title="'+temp['title']+'">'+temp['name']+'</a></li>');
+			}
+			_html.push('</ul>');
+			_html.push('<div id="menu-key" class="menu-key">>></div>');
+			html.innerHTML=_html.join('');
+			document.body.appendChild(html);
+			menuList=document.getElementById('menu-list');
+			arrow=document.getElementById('menu-key');
+			//bindevent
+			addEvent(html,'mouseenter',function(e){
+				menuList.style.width='220px';
+				arrow.innerHTML='<<';
+			});
+			addEvent(html,'mouseleave',function(){
+				menuList.style.width='0px';
+				arrow.innerHTML='>>';
+			});
 		}
 	};
 	
